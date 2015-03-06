a5e20cd80df40091d4e4e47d8f4fa87d13013461
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-05 17:06:59 -1000

    creating routes in app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,42 @@
 var express = require('express');
-var app = express();
-var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// var connect = require('connect'); **method-override
+var methodOverride = require('method-override');
+var app = express();
+
+var mongoose = require('mongoose');
+// /photo is pointing to the database name
+mongoose.connect('mongodb://localhost/photo');
+var Schema = mongoose.Schema;
+
+var photoSchema = new Schema({
+  autor: String,
+  link: String,
+  description: String,
+  created_at: Date
 });
+// lets you save and get find() photos is the collection
+var Photo = mongoose.model('Photo', photoSchema);
+
+// Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.set('view engine', 'jade');
+
+// Renders Main Gallery Page
+app.get('/', function (req, res) {
+  // calls all photos in database
+  Photo.find(function (err, photos ) {
+    if (err) throw err;
+    console.log(photos);
+    res.render('gallery', { photos : photos });
+  });
+});
+
+// Renders single gallery photo page
+// app.get('/gallery/:id', )
 
 var server = app.listen(3000, function () {
 
