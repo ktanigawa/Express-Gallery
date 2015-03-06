e2d2cf2345865ec9db2bb0791d741381790beab7
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-05 19:39:58 -1000

    postman tests passing

--- app/app.js
+++ app/app.js
@@ -31,12 +31,86 @@ app.get('/', function (req, res) {
   Photo.find(function (err, photos ) {
     if (err) throw err;
     console.log(photos);
+    // rendering jade template gallery all of the photos are passed to template
+    // for photo in photos
     res.render('gallery', { photos : photos });
   });
 });
 
-// Renders single gallery photo page
-// app.get('/gallery/:id', )
+// Renders single gallery photo page 
+app.get('/gallery/:id', function (req, res) {
+//find photo by id, cb get photo back
+  Photo.findOne({_id : req.params.id}, function (err, photo) {
+    if (err) throw err;
+// gives template photo that is found
+    res.render('gallery', { photo: photo});
+  });
+});
+
+// Renders gallery photo form 
+// url /new_photo 
+app.get('/new_photo', function (req, res) {
+  // ++new_photo.jade??
+  res.render('new_photo');
+});
+
+// Saves new photo id
+// post gallery goes here to test for making new ones
+// this route accepts new user data from the client
+app.post('/gallery', function (req, res){
+  console.log(req.body);
+  // creates a newPhoto item with that data 
+  var newPhoto = new Photo({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description,
+    created_at: new Date()
+  });
+  console.log(newPhoto);
+  newPhoto.save(function (err){
+    if (err) {
+      throw err;
+    }
+    // Redirects to root
+    res.redirect('/');
+    console.log('saved');
+  });
+});
+
+// Renders prefilled photo form
+app.get('/gallery/:id/edit', function (req, res){
+  // find photo 
+  Photo.findOne({_id : req.params.id}, function (err, photo) {
+    // ++edit_photo.jade
+    res.render('edit_photo', {photo : photo});
+  });
+});
+
+// Updates gallery photo
+app.put('/gallery/:id', function (req, res){
+  // http://mongoosejs.com/docs/api.html#query_Query-findOneAndUpdate
+  Photo.findOneAndUpdate({_id : req.params.id}, {$set: {
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  }},
+    function (err, photo){
+    if (err) throw err;
+    // Redirects to gallery/:id
+    res.redirect('/gallery/'+req.params.id);
+  });
+});
+
+
+// Delete Gallery Photo :id
+app.delete('/gallery/:id', function (req, res){
+  Photo.remove({_id: req.params.id}, function (err){
+    if (err) throw err;
+    // Redirects to '/' Root
+    res.redirect('/');
+  });
+});
+
 
 var server = app.listen(3000, function () {
 
@@ -44,5 +118,4 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
